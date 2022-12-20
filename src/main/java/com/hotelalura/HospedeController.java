f1e501f12ad31a809685fefefb3a1b16a22ff70a
guipmenezes <guimenezesunb@gmail.com>
2022-12-19 22:25:36 -0300

    RefactorÃ: Transfering the HTTP Methods to the Controller class

---
+++ src/main/java/com/hotelalura/HospedeController.java
@@ -0,0 +1,60 @@
+package com.hotelalura;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/hospede")
+public class HospedeController {
+
+    private final HospedeRepository hospedeRepository;
+
+    public HospedeController(HospedeRepository hospedeRepository) {
+        this.hospedeRepository = hospedeRepository;
+    }
+
+    @GetMapping
+    public List<Hospede> getHospede() {
+        return hospedeRepository.findAll();
+    }
+    record NewHospedeRequest(
+            Date data,
+            String nacionalidade,
+            String nome,
+            String sobrenome,
+            String telefone
+    ) {
+
+    }
+    @PostMapping
+    public void addHospede(@RequestBody NewHospedeRequest request) {
+        Hospede hospede = new Hospede();
+        hospede.setDataNascimento(request.data());
+        hospede.setNacionalidade(request.nacionalidade());
+        hospede.setNome(request.nome());
+        hospede.setSobrenome(request.sobrenome());
+        hospede.setTelefone(request.telefone());
+        hospedeRepository.save(hospede);
+    }
+
+    @DeleteMapping("{hospedeId}")
+    public void deleteHospede(@PathVariable("hospedeId") Integer id) {
+        hospedeRepository.deleteById(id);
+    }
+
+    @PutMapping("{hospedeId}")
+    public ResponseEntity<Hospede> updateCostumer(@PathVariable(value = "hospedeId") Integer id, @RequestBody NewHospedeRequest request) {
+        Hospede hospede = hospedeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Não foi possível encontrar o hóspede de id: " + id));
+        hospede.setDataNascimento(request.data());
+        hospede.setNacionalidade(request.nacionalidade());
+        hospede.setNome(request.nome());
+        hospede.setSobrenome(request.sobrenome());
+        hospede.setTelefone(request.telefone());
+        final Hospede update = hospedeRepository.save(hospede);
+        return ResponseEntity.ok(update);
+    }
+}
--- src/main/java/com/hotelalura/Main.java
+++ src/main/java/com/hotelalura/Main.java
@@ -12,59 +12,8 @@ import java.sql.Date;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("api/v1/hospede")
 public class Main {
-    private final HospedeRepository hospedeRepository;
-
-    public Main(HospedeRepository hospedeRepository) {
-        this.hospedeRepository = hospedeRepository;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    @GetMapping
-    public List<Hospede> getHospede() {
-        return hospedeRepository.findAll();
-    }
-
-    record NewHospedeRequest(
-            Date data,
-            String nacionalidade,
-            String nome,
-            String sobrenome,
-            String telefone
-    ) {
-
-    }
-    @PostMapping
-    public void addHospede(@RequestBody NewHospedeRequest request) {
-        Hospede hospede = new Hospede();
-        hospede.setDataNascimento(request.data());
-        hospede.setNacionalidade(request.nacionalidade());
-        hospede.setNome(request.nome());
-        hospede.setSobrenome(request.sobrenome());
-        hospede.setTelefone(request.telefone());
-        hospedeRepository.save(hospede);
-    }
-
-    @DeleteMapping("{hospedeId}")
-    public void deleteHospede(@PathVariable("hospedeId") Integer id) {
-        hospedeRepository.deleteById(id);
-    }
-
-    @PutMapping("{hospedeId}")
-    public ResponseEntity<Hospede> updateCostumer(@PathVariable(value = "hospedeId") Integer id, @RequestBody NewHospedeRequest request) {
-        Hospede hospede = hospedeRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Não foi possível encontrar o hóspede de id: " + id));
-        hospede.setDataNascimento(request.data());
-        hospede.setNacionalidade(request.nacionalidade());
-        hospede.setNome(request.nome());
-        hospede.setSobrenome(request.sobrenome());
-        hospede.setTelefone(request.telefone());
-        final Hospede update = hospedeRepository.save(hospede);
-        return ResponseEntity.ok(update);
     }
 }
